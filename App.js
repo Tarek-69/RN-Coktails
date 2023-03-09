9be59cfc20987b372e0bdf3115b2ea172c014344
Tarek <t.taguine@it-students.fr>
2023-03-09 11:45:05 +0100

    affichage des données

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import axios from "axios";
 
-export default function App() {
+const App = () => {
+  const [cocktails, setCocktails] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a")
+      .then((response) => {
+        setCocktails(response.data.drinks);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Liste des cocktails:</Text>
+      {cocktails.map((cocktail) => (
+        <Text key={cocktail.idDrink}>{cocktail.strDrink}</Text>
+      ))}
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
